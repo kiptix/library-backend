6122eb0b9637fbbf5d6b960e49ea56b7cefec6af
jejansse <jens.janssen@btc-ag.com>
2017-05-05 14:13:09 +0200

    bookcontroller

---
+++ src/main/kotlin/de/btc/library/BookController.kt
@@ -0,0 +1,16 @@
+package de.btc.library
+
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/books")
+class BookController(val bookRepository: BookRepository) {
+    @GetMapping("/{id}/lend")
+    fun lend(@PathVariable("id") id: Long, @RequestParam("user") user: String) {
+        val book  = bookRepository.findById(id)
+        book.ifPresent({
+            it.user = user
+            bookRepository.save(it)
+        })
+    }
+}
