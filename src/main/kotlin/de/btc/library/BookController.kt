af43aefb2dc42be65e6fd2bc3fb3607c63dd9780
Stefan Meyer <st.meyer@uni-oldenburg.de>
2017-05-05 15:28:57 +0200

    loginseite eingebaut

--- src/main/kotlin/de/btc/library/BookController.kt
+++ src/main/kotlin/de/btc/library/BookController.kt
@@ -6,11 +6,12 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/books")
 class BookController(val bookRepository: BookRepository) {
     @GetMapping("/{id}/lend")
-    fun lend(@PathVariable("id") id: Long, @RequestParam("user") user: String) {
+    fun lend(@PathVariable("id") id: Long, @RequestParam("user") user: String) :String{
         val book  = bookRepository.findById(id)
         book.ifPresent({
             it.user = user
             bookRepository.save(it)
         })
+        return "ok";
     }
 }
